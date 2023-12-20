008bbdb9ebb5b1e20277bb076d07e86f04bc3d12
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-20 15:46:16 +0530

    DisplayName

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './App.css'; // You can create this CSS file for additional styling
+
+const App = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (firstName.trim() !== '' && lastName.trim() !== '') {
+      setFullName(`${firstName} ${lastName}`);
+    } else {
+      
+      alert('Please fill in both first and last names.');
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96"
+      >
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
+            First Name:
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="firstName"
+            type="text"
+            placeholder="Enter your first name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
+            Last Name:
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="lastName"
+            type="text"
+            placeholder="Enter your last name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Submit
+        </button>
+      </form>
+
+      {fullName && (
+        <div className="bg-white shadow-md rounded p-4 mb-4">
+          <h2 className="text-xl font-bold mb-2">Full Name:</h2>
+          <p className="text-gray-800">{fullName}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
